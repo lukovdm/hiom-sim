4a6092f2325cfc62217a9f7c664724c33e2f5b55
hvdmaas <hvandermaas@gmail.com>
2020-05-24 20:15:17 +0200

    added SO

--- docs/bootstrap.js
+++ docs/bootstrap.js
@@ -174,7 +174,7 @@
 /******/ 				promises.push(installedWasmModuleData);
 /******/ 			else {
 /******/ 				var importObject = wasmImportObjects[wasmModuleId]();
-/******/ 				var req = fetch(__webpack_require__.p + "" + {"../pkg/hiom_sim_bg.wasm":"0a65d6a6507dc1997f08"}[wasmModuleId] + ".module.wasm");
+/******/ 				var req = fetch(__webpack_require__.p + "" + {"../pkg/hiom_sim_bg.wasm":"e2e6fc817a7aeb747679"}[wasmModuleId] + ".module.wasm");
 /******/ 				var promise;
 /******/ 				if(importObject instanceof Promise && typeof WebAssembly.compileStreaming === 'function') {
 /******/ 					promise = Promise.all([WebAssembly.compileStreaming(req), importObject]).then(function(items) {
